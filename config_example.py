523f27b91332ab46080a695023f8411c73251119
chenxuan <3309003591@qq.com>
2020-04-27 17:05:02 +0800

    增加配置文件说明

---
+++ config_example.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+from nonebot.default_config import *
+#全局管理员
+SUPERUSERS.add(3309003591)
+#命令起始标识
+COMMAND_START = {'!','！'}
+#bot称呼-暂无作用
+NICKNAME = {'bot', 'bot哥', '工具人', '最菜群友'}
+
+#默认botQQ 重要信息及未分类信息将推送至此QQ，为空可能导致错误
+default_bot_QQ = 
+#bot错误信息推送到的Q号，为空时不进行推送
+bot_error_printID = 
+#填写twitter提供的开发Key和secret
+consumer_key = '********************'
+consumer_secret = '********************'
+access_token = '********************-********************'
+access_token_secret = '********************'
