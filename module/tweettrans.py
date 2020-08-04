f39409597ec50667c088130716a969da9db84f65
chenxuan <3309003591@qq.com>
2020-08-04 13:31:31 +0800

    尝试修复烤推英文界面的bug

--- module/tweettrans.py
+++ module/tweettrans.py
@@ -39,6 +39,7 @@ class TweetTrans:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--lang=zh_CN')
+        chrome_options.add_argument('accept-language=zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6')
         #chrome_options.add_argument('--disk-cache-dir='+os.path.join('.','cache','chromecache'))
         chrome_options.add_argument("user-agent="+randUserAgent())
         self.driver = webdriver.Chrome(options=chrome_options)
