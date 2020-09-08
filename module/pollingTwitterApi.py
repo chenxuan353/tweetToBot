c9d71776015949b88056e270430915c1b3a7e0d0
chenxuan <3309003591@qq.com>
2020-09-08 21:11:48 +0800

    修复重启不推送的BUG

--- module/pollingTwitterApi.py
+++ module/pollingTwitterApi.py
@@ -179,7 +179,7 @@ def get_updata(trigger : bool = True,start = False):
                 twitter.tweetcache.addTweetToCache(res['tweetinfo'])
             # 推文不存在于缓存时推送
             if cache:
-                if trigger or start:
+                if trigger or (trigger and start):
                     on_status(statuss[i])
             elif trigger:
                 try:
